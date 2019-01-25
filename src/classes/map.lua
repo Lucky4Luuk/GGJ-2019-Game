106e3581724a10a68a7d7d290a882d180591afc4
Luuk van Oijen <lazyluuk.channel@gmail.com>
2019-01-25 23:18:35 +0100

    yeet yeet

---
+++ src/classes/map.lua
@@ -0,0 +1,34 @@
+local map = {_TYPE='module', _NAME='map', _VERSION='0.1'}
+local map_meta = {}
+
+function map:new()
+  --Make map
+  local m = {levels={}, wires={}, type="map"}
+
+  --Metatable stuff
+  return setmetatable(m, map_meta)
+end
+
+function map.get_wires(self) --fucking useless like me
+  return self.wires
+end
+
+function map.add_wire(self, to, from)
+  --return setmetatable(map_result, map_meta)
+  table.insert(self.wires, {to=to, from=from})
+end
+
+setmetatable( map, { __call = function( ... ) return map.new( ... ) end } )
+
+--Print print()
+map_meta.__call = function(...)
+  return map.print(...)
+end
+
+--Indexing
+map_meta.__index = {}
+for k,v in pairs(map) do
+  map_meta.__index[k] = v
+end
+
+return map
---
+++ src/main.lua
@@ -0,0 +1,23 @@
+local map = require("classes.map")
+
+local m = map:new()
+
+--Timer
+local total_time = 0
+local fixed_delta_time = 1/240
+
+function love.update(dt)
+  total_time = total_time + dt
+  while fixed_delta_time > total_time do
+    fixed_update()
+    total_time = total_time - fixed_delta_time
+  end
+end
+
+function fixed_update()
+
+end
+
+function love.draw()
+  --no
+end
